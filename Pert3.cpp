bb42600c82de4b961e42e29b7ae8fbce34fc521e
Lardzzz <aku.ikan04@gmail.com>
2023-03-15 16:24:31 +0700

    Membuat program dengan kondisi if else

--- Pert3.cpp
+++ Pert3.cpp
@@ -40,7 +40,5 @@ int main(){
     cout << "Nilai rata-ratanya : " << rerata(NilMath, NilBIndo);
     cout << "\n Status Kelulusan : " << status(rerata(NilMath, NilBIndo));
     cout << "\n Status Kelulusan : " << status2(rerata(NilMath, NilBIndo), NilMath);
-    cout << "\n Status Kelulusan : " << status3(rerata(NilMath, NilBIndo), NilMath);
-
-    
+    cout << "\n Status Kelulusan : " << status3(rerata(NilMath, NilBIndo), NilMath);  
 }
